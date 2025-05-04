6639381c7e24687e9ed1b0644a8402b89332cdbf
Ekaterina Koloyarskaya <KoloyarskayaEk@yandex.ru>
2025-05-04 21:17:52 +0300

    Поработать с именованием. Разместить код в верных модулях. Поправить утечку памяти при работе с закрытием по эскейпу. Удалить в разметке подключения скриптов.

--- src/scripts/cards.js
+++ src/scripts/cards.js
@@ -1,44 +1,38 @@
-
-
 // массив объекта, который в свою очередь имеет по 2 свойства
 
 export const initialCards = [
-    {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-    },
-    {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-    },
-    {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-    },
-    {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-    },
-    {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-    },
-    {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-    }
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
 ];
-// Функции для работы с карточками проекта Mesto вынесите в файл card.js, из него должна экспортироваться 
-// функция initialCards. Функции, обрабатывающие события лайка и удаления карточки, 
-// также должны находиться в этом файле и экспортироваться из него.
 
-// функция для смены класса сердца в активное состояние
-export function cardLikeButton(likeButton) {
-  likeButton.classList.toggle('card__like-button_is-active');
-  };
-
-export function addCard(cardData, removeCardCallback, imageModalClick, cardTemplate) {
-
+export function addCard(
+  cardData,
+  removeCardCallback,
+  imageModalClick,
+  cardTemplate
+) {
   // клонируем содержимое тега template
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
 
@@ -48,25 +42,36 @@ export function addCard(cardData, removeCardCallback, imageModalClick, cardTempl
   const cardTitle = cardElement.querySelector(".card__title");
   const likeButton = cardElement.querySelector(".card__like-button");
 
-  // добавляем атрибуты тегам и присваиваем им значения из объека 
+  // добавляем атрибуты тегам и присваиваем им значения из объека
   cardImage.src = cardData.link;
   cardImage.alt = cardData.name;
   cardTitle.textContent = cardData.name;
 
-  // удаляется вся карточка со всем ее содержимым после того, как нажали на кнопку удаления 
-  cardDeleteButton.addEventListener("click", () => removeCardCallback(cardElement));
+  // удаляется вся карточка со всем ее содержимым после того, как нажали на кнопку удаления
+  cardDeleteButton.addEventListener("click", () =>
+    removeCardCallback(cardElement)
+  );
 
   // клик по карточке, чтобы вызвать модально окно
-  cardImage.addEventListener('click', () => imageModalClick(cardData.link, cardData.name));
-  
+  cardImage.addEventListener("click", () =>
+    imageModalClick(cardData.link, cardData.name)
+  );
+
   // клик по кнопке сердца
-  likeButton.addEventListener('click', () => {
+  likeButton.addEventListener("click", () => {
     cardLikeButton(likeButton);
   });
-  
+
   return cardElement;
 }
 
+// функция  удаляет переданный элемент (cardElement) из DOM.
+export function removeCard(cardElement) {
+  cardElement.remove();
+}
 
-
+// функция для лайка крточки(сердце активно)
+export function cardLikeButton(likeButton) {
+  likeButton.classList.toggle("card__like-button_is-active");
+}
 
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,28 @@
+import "../pages/index.css";
+import { initialCards } from "./cards.js";
+import { removeCard } from "./cards.js";
+import { openModal } from "../scripts/modal.js";
+import { closeModal } from "../scripts/modal.js";
+import { addCard } from "./cards.js";
+import { closeModalOverlayListener } from "./modal.js";
 
-import '../pages/index.css';
-import { initialCards } from './cards.js';
-import { openModal } from '../scripts/modal.js';
-import { closeModal } from '../scripts/modal.js';
-import { addCard } from './cards.js';
-
-
+// создаю переменные, которые будут находить нужные классы
+const profileEditButton = document.querySelector(".profile__edit-button");
+const popupTypeNewCard = document.querySelector(".popup_type_new-card");
+const profileAddButton = document.querySelector(".profile__add-button");
+const closePopupTypeEdit = document.querySelector(
+  ".popup_type_edit .popup__close"
+);
+const closePopupTypeNewCard = document.querySelector(
+  ".popup_type_new-card .popup__close"
+);
+const popupTypeImage = document.querySelector(".popup_type_image");
+const closePopupTypeImage = document.querySelector(
+  ".popup_type_image .popup__close"
+);
+const allPopups = document.querySelectorAll(".popup");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
 
 // получаем содержимое template
 export const cardTemplate = document.querySelector("#card-template").content;
@@ -13,43 +30,128 @@ export const cardTemplate = document.querySelector("#card-template").content;
 // ищем в документе блок с классом places__list
 export const cardContainer = document.querySelector(".places__list");
 
-// функция  удаляет переданный элемент (cardElement) из DOM.
-export function removeCard(cardElement) {
-  cardElement.remove();
-}
-
 // создаёт карточки из списка initialCards и добавляет их в контейнер на странице.
 initialCards.forEach((cardData) => {
-  const cardElement = addCard(cardData, removeCard, imageModalClick, cardTemplate);
+  const cardElement = addCard(
+    cardData,
+    removeCard,
+    imageModalClick,
+    cardTemplate
+  );
   cardContainer.append(cardElement);
 });
 
-
 // функция открытия модального окна на карточку
 export function imageModalClick(link, name) {
-  const windowModal = document.querySelector(".popup_type_image");
-  const cardImage = windowModal.querySelector(".popup__image");
-  const cardTitle = windowModal.querySelector(".popup__caption");
-  
+  const editProfileForm = document.querySelector(".popup_type_image");
+  const cardImage = editProfileForm.querySelector(".popup__image");
+  const cardTitle = editProfileForm.querySelector(".popup__caption");
+
   cardImage.src = link;
   cardImage.alt = name;
   cardTitle.textContent = name;
-  openModal(windowModal);
+  openModal(editProfileForm);
 }
 
+// клик по кнопке редактирвоания
+profileEditButton.addEventListener("click", () => {
+  // Заполненные значения профиля
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileDescription.textContent;
+  openModal(editProfileForm);
+});
 
+// клик по плюсику
+profileAddButton.addEventListener("click", () => {
+  openModal(popupTypeNewCard);
+});
 
+// клик по крестику закрытия у модал окна на кнопку редактирования
+closePopupTypeEdit.addEventListener("click", () => {
+  closeModal(editProfileForm);
+});
 
+// клик по крестику закрытия у модал окна +
+closePopupTypeNewCard.addEventListener("click", () => {
+  closeModal(popupTypeNewCard);
+});
 
+// клик по крестику закрытия у модал окна картинок
+closePopupTypeImage.addEventListener("click", () => {
+  closeModal(popupTypeImage);
+});
 
+// // Работа с формой "Редактировать профиль"
+// Находим форму в DOM
+const editProfileForm = document.querySelector(".popup_type_edit");
+const formElementEdit = editProfileForm.querySelector(".popup__form");
 
+// Находим поля формы в DOM
+const nameInput = formElementEdit.querySelector(".popup__input_type_name");
+const jobInput = formElementEdit.querySelector(
+  ".popup__input_type_description"
+);
 
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function handleFormSubmitEditProfile(evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  // Так мы можем определить свою логику отправки.
+  // О том, как это делать, расскажем позже.
 
-// В файле index.js должны остаться:
-// - объявления и инициализация глобальных констант и переменных с DOM-элементами страницы,
-// - обработчики событий (при открытии и закрытии попапов; при отправке форм; обработчик, 
-//   открывающий попап при клике по изображению карточки);
-// - вызовы других функций, подключённых из созданных модулей, которым нужно будет передавать 
-// объявленные здесь переменные и обработчики.
+  // Получите значение полей jobInput и nameInput из свойства value
+  const nameInputValue = nameInput.value;
+  const jobInputValue = jobInput.value;
 
+  // Вставьте новые значения с помощью textContent
+  profileTitle.textContent = nameInputValue;
+  profileDescription.textContent = jobInputValue;
+}
 
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElementEdit.addEventListener("submit", handleFormSubmitEditProfile);
+
+// Работа с формой "Новое место"
+// Нахожу форму в DOM
+const addCardModal = document.querySelector(".popup_type_new-card");
+const formElementCard = addCardModal.querySelector(".popup__form");
+
+// Нахожу поля формы в DOM
+const placeInput = formElementCard.querySelector(
+  ".popup__input_type_card-name"
+);
+const linkImageInput = formElementCard.querySelector(".popup__input_type_url");
+
+// Обработчик «отправки» формы, хотя пока // она никуда отправляться не будет
+export function handleFormSubmitPlace(evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  // Так мы можем определить свою логику отправки.  // О том, как это делать, расскажем позже.
+
+  // Получаю значение полей placeInput и linkImageInput из свойства value
+  const placeInputValue = placeInput.value;
+  const linkImageInputValue = linkImageInput.value;
+
+  const cardData = {
+    name: placeInput.value,
+    link: linkImageInput.value,
+  };
+
+  // Создаю карточку и добавляю ее в контейнер
+  const createCard = addCard(
+    cardData,
+    removeCard,
+    imageModalClick,
+    cardTemplate
+  );
+  cardContainer.prepend(createCard);
+
+  // Закрываю модальное окно и Очищаю форму
+  closeModal(popupTypeNewCard);
+  formElementCard.reset();
+}
+// Прикрепляю обработчик к форме: // он будет следить за событием “submit” - «отправка»
+formElementCard.addEventListener("submit", handleFormSubmitPlace);
+
+// вызовы закрытия
+closeModalOverlayListener(allPopups);
